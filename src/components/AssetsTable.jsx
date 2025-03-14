e9e0b7449a5bff46118461f67165e046dbe4f8db
ML <mirraxgg@gmail.com>
2025-03-14 18:53:26 +0300

    finished

--- src/components/AssetsTable.jsx
+++ src/components/AssetsTable.jsx
@@ -1,5 +1,41 @@
 import { Table } from "antd";
+import { useCrypto } from "../context/cryptocontext";
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    sorter: (a, b) => a.name.length - b.name.length,
+    sortDirections: ["descend"],
+  },
+  {
+    title: "Price, $",
+    dataIndex: "price",
+    defaultSortOrder: "descend",
+    sorter: (a, b) => a.price - b.price,
+  },
+  {
+    title: "Amount",
+    dataIndex: "amount",
+    defaultSortOrder: "descend",
+    sorter: (a, b) => a.amount - b.amount,
+  },
+];
 
 export default function AssetsTable() {
-  return <div>112</div>;
+  const { assets } = useCrypto();
+  const data = assets.map((a) => ({
+    key: a.id,
+    name: a.name,
+    price: a.price,
+    amount: a.amount,
+  }));
+  return (
+    <Table
+      columns={columns}
+      dataSource={data}
+      /*onChange={onChange}*/
+      pagination={false}
+    />
+  );
 }
